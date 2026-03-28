4aa23ba5a611c768bef47f8c4173620327d67a5e
TheBlueHawk44 <132791743+TheBlueHawk44@users.noreply.github.com>
2026-03-27 20:44:29 -0400

    initial SpringBoot Package

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+@RestController
+@CrossOrigin(origins = "*")
+public class HelloController {
+    private final UserRepository userRepository;
+    public HelloController(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+    @PostMapping("/user/add")
+    public User addUser(@RequestBody User user){
+        return userRepository.save(user);
+    }
+    @GetMapping("/users")
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+   @GetMapping("/")
+    public String index(){
+       return "Spring is officially working!";
+   }
+    @GetMapping("/user")
+   public User getUser(){
+        return new User("Owen", 92374);
+    }
+}
