14287d2743dbe40040195fba15442bcd1f0ae778
Leonardo de Farias <71597308+Tofudog@users.noreply.github.com>
2026-08-21 16:06:48 -0400

    Change to est (#49)

--- src/main/java/com/example/demo/SpringExperienceApplication.java
+++ src/main/java/com/example/demo/SpringExperienceApplication.java
@@ -1,4 +1,6 @@
 package com.example.demo;
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringExperienceApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/New_York"));
 		SpringApplication.run(SpringExperienceApplication.class, args);
 	}
 
--- src/main/java/com/example/demo/event/EventRepository.java
+++ src/main/java/com/example/demo/event/EventRepository.java
@@ -13,6 +13,6 @@ public interface EventRepository extends JpaRepository<Event, String> {
     List<Event> findAllByOrderByStartTimeAsc();
 
     Optional<Event> findFirstByEndTimeGreaterThanEqualOrderByStartTimeAsc(LocalDateTime time);
-    @Query(value = "SELECT * FROM events WHERE start_time >= NOW() AND start_time <= NOW() + INTERVAL '30 minutes' ORDER BY start_time ASC", nativeQuery = true)
+    @Query(value = "SELECT * FROM events WHERE start_time >= (NOW() AT TIME ZONE 'America/New_York') AND start_time <= (NOW() AT TIME ZONE 'America/New_York') + INTERVAL '30 minutes' ORDER BY start_time ASC", nativeQuery = true)
     List<Event> findUpcomingEventsInNext30Minutes();
 }
