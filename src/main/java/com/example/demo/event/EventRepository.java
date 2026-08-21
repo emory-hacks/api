f87d2c59ac3dffbaba8e3179af6c73a9d3ad21df
Leonardo de Farias <71597308+Tofudog@users.noreply.github.com>
2026-08-21 14:41:53 -0400

    Added /schedule/upcoming functionality (#48)
    Co-authored-by: TheBlueHawk44 <132791743+TheBlueHawk44@users.noreply.github.com>

--- src/main/java/com/example/demo/event/EventRepository.java
+++ src/main/java/com/example/demo/event/EventRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,4 +13,6 @@ public interface EventRepository extends JpaRepository<Event, String> {
     List<Event> findAllByOrderByStartTimeAsc();
 
     Optional<Event> findFirstByEndTimeGreaterThanEqualOrderByStartTimeAsc(LocalDateTime time);
+    @Query(value = "SELECT * FROM events WHERE start_time >= NOW() AND start_time <= NOW() + INTERVAL '30 minutes' ORDER BY start_time ASC", nativeQuery = true)
+    List<Event> findUpcomingEventsInNext30Minutes();
 }
